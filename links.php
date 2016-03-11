79700a1baea3d7e4936bff3f42af0e906724c32c
grandmasteriVan <grandmaster.ivan@gmail.com>
2016-03-11 12:41:54 +0200

    tags

--- links.php
+++ links.php
@@ -5,11 +5,12 @@
  * Date: 10.03.16
  * Time: 09:14
  */
+
+
 function add_tags($text)
 {
-    //preg_match_all("/<a href=*\/a>/",$text,$links);
 	preg_match_all("/<a(.*?)<\/a>/",$text,$links);
-	print_r ($links);
+	//print_r ($links);
     foreach ($links[0] as $link)
     {
         //echo $link;
@@ -20,7 +21,7 @@ function add_tags($text)
             $tegpos=strpos($link,">");
             $newLink=substr_replace($link,' rel="nofolow"',$tegpos,0);
             $newLink="<noindex>".$newLink."</noindex>";
-			echo " ".$link." to ".$newLink ;
+			//echo " ".$link." to ".$newLink ;
 			
 			
 			$text=str_replace ($link,$newLink,$text);
@@ -30,11 +31,37 @@ function add_tags($text)
 	return $text;
 }
 
+function db_correction ()
+{
+    $db_connect=mysqli_connect('localhost','root','','ddn');
+    $query="SELECT news_id, news_content FROM news";
+    if ($res=mysqli_query($db_connect,$query))
+    {
+        while ($row=mysqli_fetch_assoc($res))
+        {
+            $arr[]=$row;
+        }
+        foreach ($arr as $row)
+        {
+            $text1=$row['news_content'];
+            $id=$row['news_id'];
+            $text2=add_tags($text1);
+            if ($text1!=$text2)
+            {
+                $query="UPDATE news ".
+                    "SET news_content=$text2".
+                    "WHERE news_id=$id";
+                mysqli_query($query,$db_connect);
+            }
+        }
+    }
+}
+
 $text='ddd <a href="f.com">link</a> fff <a href="c.com">link2</a> hjhg <a href="divani.kiev.ua/links.html">divani</a> some text';
 //echo $text;
-$tmp= add_tags($text);
-echo " new text: ";
-echo $tmp;
+$newText= add_tags($text);
+//echo " new text: ";
+//echo $newText;
 
 
 ?>
