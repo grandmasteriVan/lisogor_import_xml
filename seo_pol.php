3d1a073e3b1ab0943ac08da7a32f46f9a24fa8a8
grandmasteriVan <grandmaster.ivan@gmail.com>
2016-09-15 12:37:23 +0300

    add + new

--- seo_pol.php
+++ seo_pol.php
@@ -22,6 +22,47 @@ define ("pass", "Z7A8JqUh");
  */
 //define ("db", "mebli");
 define ("db", "uh333660_mebli");
+
+/**
+ * заполняет СЕО поля по фабрике Velam
+ */
+function seo_velam_matr()
+{
+    $db_connect=mysqli_connect(host,user,pass,db);
+    $query="SELECT goods_id FROM goods WHERE goodskind_id=35 AND factory_id=137";
+    if ($res=mysqli_query($db_connect,$query))
+    {
+        while ($row = mysqli_fetch_assoc($res))
+        {
+            $goods[]=$row;
+        }
+        foreach ($goods as $good)
+        {
+            $id=$good['goods_id'];
+            $name=$good['goods_name'];
+
+            $header=$good['goods_name'];
+            $name_trunc=str_replace(UTF8toCP1251("Матрас "),"",$name);
+            $title=$name_trunc.UTF8toCP1251(" матрас. Купить матрасы со склада в Киеве");
+            $keywords=UTF8toCP1251("матрасы, ").$name.UTF8toCP1251(", склад мебели, купить матрас, интернет магазин мебели, недорогие матрасы, цены, фото, отзывы.");
+            $key_h=UTF8toCP1251("Фабрика Velam. ").$name.UTF8toCP1251(".  Характеристики, фото, цена, отзывы. Купить недорого со склада в Киеве. Доставка по Украине.");
+            $key_f=UTF8toCP1251("Фабрика Velam. ").$name.UTF8toCP1251(". Характеристики, фото, ціна, відгуки. Купити недорого зі складу в Києві. Доставка по Україні.");
+            $desc=UTF8toCP1251("Купить ").$name.UTF8toCP1251(" в интернет магазине «Файні-меблі», Киев. Большой склад выставка в Киеве. Доставка по Украине, гарантия, лучшие цены.");
+
+            $query="UPDATE goods SET goods_header='$header', goods_title='$title', goods_keyw='$keywords', goods_hkeyw='$key_h', goods_fkeyw='$key_f', goods_desc='$desc' WHERE goods_id=$id";
+            mysqli_query($db_connect,$query);
+            echo $query."<br>";
+            //$header=$good['goods_header'];
+            /*$title=$good['goods_title'];
+            $keywords=$good['goods_keyw'];
+            $key_h=$good['goods_hkeyw'];
+            $key_f=&$good['goods_fkeyw'];
+            $desc=$good['goods_desc'];*/
+            //break;
+        }
+    }
+    mysqli_close($db_connect);
+}
 /**
  *заполняет СЕО поля по фабрике ДОМ
  */
@@ -170,7 +211,8 @@ function seo_mks()
 $time_start = microtime(true);
 //seo_kupe_dom();
 //seo_mks();
-seo_kiev_sofievka();
+//seo_kiev_sofievka();
+seo_velam_matr();
 $time_end = microtime(true);
 $time = $time_end - $time_start;
 echo "Runtime: $time sec\n";
