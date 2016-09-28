26b720c45fd0d90dbb5f37c19cad231321095429
grandmasteriVan <grandmaster.ivan@gmail.com>
2016-09-28 15:40:49 +0300

    Update lisogor_plus.php

--- factory/lisogor_plus.php
+++ factory/lisogor_plus.php
@@ -5,28 +5,27 @@
  * Date: 28.09.16
  * Time: 15:25
  */
-
 /**
  * database host
  */
-define ("host","localhost");
-//define ("host","10.0.0.2");
+//define ("host","localhost");
+define ("host","10.0.0.2");
 /**
  * database username
  */
-define ("user", "root");
-//define ("user", "uh333660_mebli");
+//define ("user", "root");
+define ("user", "uh333660_mebli");
 /**
  * database password
  */
-define ("pass", "");
-//define ("pass", "Z7A8JqUh");
+//define ("pass", "");
+define ("pass", "Z7A8JqUh");
 /**
  * database name
  */
-define ("db", "mebli");
-//define ("db", "uh333660_mebli");
-
+//define ("db", "mebli");
+define ("db", "uh333660_mebli");
+$db_connect=mysqli_connect(host,user,pass,db);
 for ($i=1;$i<=10;$i++)
 {
     $cat_id=627+$i;
@@ -35,12 +34,13 @@ for ($i=1;$i<=10;$i++)
         "WHERE goodshascategory.goods_id IN ".
         "(SELECT goods_id FROM goods WHERE (goods.factory_id=66)) ".
         "AND (goodshascategory.category_id=$cat_id)";
-    //echo $strSQL."<br>";
+    echo $strSQL."<br>";
     //break;
     mysqli_query($db_connect, $strSQL);
 }
 //записываем цену первой категории в таблицу goods
 $strSQL="UPDATE goods SET goods_price=goods_price+10 WHERE factory_id=66";
+echo $strSQL."<br>";
 mysqli_query($db_connect, $strSQL);
-
+mysqli_close($db_connect);
 ?>
