8e8aaa824d8390d25164f0d869fe8a3259353704
grandmasteriVan <grandmaster.ivan@gmail.com>
2017-01-09 11:45:57 +0200

    new

---
+++ action_price_check.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 09.01.17
+ * Time: 10:56
+ */
+
+//define ("host","localhost");
+define ("host","10.0.0.2");
+/**
+ * database username
+ */
+//define ("user", "root");
+define ("user", "uh333660_mebli");
+/**
+ * database password
+ */
+//define ("pass", "");
+define ("pass", "Z7A8JqUh");
+/**
+ * database name
+ */
+//define ("db", "mebli");
+define ("db", "uh333660_mebli");
+
+/**
+ * Class Timer
+ */
+class Timer
+{
+    /**
+     * @var время начала выпонения
+     */
+    private $start_time;
+    /**
+     * @var время конца выполнения
+     */
+    private $end_time;
+    /**
+     * встанавливаем время начала выполнения скрипта
+     */
+    public function setStartTime()
+    {
+        $this->start_time = microtime(true);
+    }
+    /**
+     * устанавливаем время конца выполнения скрипта
+     */
+    public function setEndTime()
+    {
+        $this->end_time = microtime(true);
+    }
+    /**
+     * @return mixed время выполения
+     * возвращаем время выполнения скрипта в секундах
+     */
+    public function getRunTime()
+    {
+        return $this->end_time-$this->start_time;
+    }
+}
+
+class Check
+{
+    private function getWrongActionList()
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $query="SELECT * FROM goods WHERE goods_price<goods_oldprice";
+        if ($res=mysqli_query($db_connect,$query))
+        {
+            while ($row = mysqli_fetch_assoc($res))
+            {
+                $goods[] = $row;
+            }
+            mysqli_close($db_connect);
+            return $goods;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public function checkActions()
+    {
+        $goods=$this->getWrongActionList();
+        $db_connect=mysqli_connect(host,user,pass,db);
+        if (is_array($goods))
+        {
+            foreach ($goods as $good)
+            {
+                $id=$goods['goods_id'];
+                $query="UPDATE goods SET goods_oldprice=0 WHERE goods_id=$id";
+                //mysqli_query($db_connect,$query);
+                echo "$query<br>";
+            }
+        }
+
+        mysqli_close($db_connect);
+    }
+}
+
+$runtime = new Timer();
+$runtime->setStartTime();
+$test=new Check();
+$test->checkActions();
+$runtime->setEndTime();
+echo "<br> runtime=".$runtime->getRunTime()." sec <br>";
