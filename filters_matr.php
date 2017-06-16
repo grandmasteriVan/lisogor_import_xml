152d82ff809319d41eb55550eeca4f3356df0541
grandmasteriVan <grandmaster.ivan@gmail.com>
2017-06-16 15:30:06 +0300

    Update filters_matr.php

--- filters_matr.php
+++ filters_matr.php
@@ -5,55 +5,30 @@
  * Date: 01.06.16
  * Time: 10:22
  */
-define ("host","localhost");
-//define ("host","10.0.0.2");
+ header('Content-type: text/html; charset=UTF-8');
+//define ("host","localhost");
+define ("host","10.0.0.2");
 /**
  * database username
  */
-define ("user", "root");
-//define ("user", "uh333660_mebli");
+//define ("user", "root");
+define ("user", "uh333660_mebli");
 /**
  * database password
  */
-define ("pass", "");
-//define ("pass", "Z7A8JqUh");
+//define ("pass", "");
+define ("pass", "Z7A8JqUh");
 /**
  * database name
  */
-define ("db", "mebli");
-//define ("db", "uh333660_mebli");
+//define ("db", "mebli");
+define ("db", "uh333660_mebli");
 //TODO: сделать универсальный скрипт, для этого в зависимости от типа товара надо копировать нужнные фильтры и сеополя
-
 /**
  * Class setFilters
  */
 class setFilters
 {
-    /**
-     * @param int $goods_maintcharter
-     * @return array|bool
-     */
-    function all_matr($goods_maintcharter=14)
-    {
-        $db_connect=mysqli_connect(host,user,pass,db);
-        $query="SELECT * FROM goods WHERE goods_maintcharter=$goods_maintcharter AND goods_active=1 AND goods_noactual=0";
-        if ($res=mysqli_query($db_connect,$query))
-        {
-            while ($row = mysqli_fetch_assoc($res))
-            {
-                $arr[] = $row;
-                //print_r($arr);
-            }
-        }
-        else
-        {
-            echo "ERROR in SQL!";
-            return false;
-        }
-        mysqli_close($db_connect);
-        return $arr;
-    }
-
     /**
      * @param int $goods_maintcharter
      * @return array
@@ -72,7 +47,6 @@ class setFilters
         mysqli_close($db_connect);
         return $arr;
     }
-
     /**
      * @param int $goods_maintcharter
      * @return array
@@ -94,7 +68,6 @@ class setFilters
         echo "</pre>";*/
         return $arr;
     }
-
     /**
      * @param $filters
      * @param $goods_id
@@ -109,7 +82,6 @@ class setFilters
         /*echo "<pre>";
         print_r ($feature_id);
         echo "</pre>";*/
-
         if (!in_array(33,$feature_id))
         {
             echo "В товаре с ид=$goods_id не проставлена нагрузка на место!<br>";
@@ -136,7 +108,6 @@ class setFilters
         }
         return;
     }
-
     private function setFilter ($goods_id,$feature_id,$goodshasfeature_valueint)
     {
         $db_connect=mysqli_connect(host,user,pass,db);
@@ -153,8 +124,10 @@ class setFilters
     public function copyFilters()
     {
         $db_connect=mysqli_connect(host,user,pass,db);
-        $parent_list=$this->parrentMatr();
+        //return;
+		$parent_list=$this->parrentMatr();
         $mod_list=$this->modMatr();
+		//return;
         foreach ($parent_list as $parent)
         {
             $parent_id=$parent['goods_id'];
@@ -175,13 +148,8 @@ class setFilters
             {
                 $goodshasfeature_valueint=2;
             }
-            $query="INSERT INTO goodshasfeature (goodshasfeature_valueint, goodshasfeature_valuefloat, ".
-                "goodshasfeature_valuetext, goods_id, feature_id) ".
-                "VALUES ($goodshasfeature_valueint, 0, ".
-                "'', $parent_id, 131)";
-            mysqli_query($db_connect,$query);
+            $this->setFilter($parent_id,131,$goodshasfeature_valueint);
             //echo "<br><br>установили размер родителя: ".$query."<br>";
-
             //выставляем фильтр тип матраса-тонкие
             $query="DELETE FROM goodshasfeature WHERE goods_id=$parent_id AND feature_id=52 AND goodshasfeature_valueint=25";
             mysqli_query($db_connect,$query);
@@ -190,20 +158,14 @@ class setFilters
             if ($size_h<=80)
             {
                 $goodshasfeature_valueint=25;
+				$this->setFilter($parent_id,52,$goodshasfeature_valueint);
             }
-            $query="INSERT INTO goodshasfeature (goodshasfeature_valueint, goodshasfeature_valuefloat, ".
-                "goodshasfeature_valuetext, goods_id, feature_id) ".
-                "VALUES ($goodshasfeature_valueint, 0, ".
-                "'', $parent_id, 52)";
-            mysqli_query($db_connect,$query);
             //echo "<br><br>установили размер родителя: ".$query."<br>";
-
             //все матрасы - ортопедические!
             $query="DELETE FROM goodshasfeature WHERE goods_id=$parent_id AND feature_id=52 AND goodshasfeature_valueint=22";
             mysqli_query($db_connect,$query);
             //echo $query."<br>";
             $this->setFilter($parent_id,52,22);
-
             //выбираем список фич для родительского товара
             $query="SELECT * FROM goodshasfeature WHERE goods_id=$parent_id";
             if ($res=mysqli_query($db_connect,$query))
@@ -240,7 +202,8 @@ class setFilters
                     //для каждой фичи записываем ее в БД
                     foreach ($features as $feat)
                     {
-                        $goodshasfeature_valueint=$feat['goodshasfeature_valueint'];
+                        //echo "копируем фильтры<br>";
+						$goodshasfeature_valueint=$feat['goodshasfeature_valueint'];
                         $goodshasfeature_valuefloat=$feat['goodshasfeature_valuefloat'];
                         $goodshasfeature_valuetext=$feat['goodshasfeature_valuetext'];
                         $feature_id=$feat['feature_id'];
@@ -259,15 +222,17 @@ class setFilters
                             {
                                 $goodshasfeature_valueint=2;
                             }
+							
                         }
                         //не пишем ненужные значния
                         if ($feature_id==33||$feature_id==52||$feature_id==53||$feature_id==55||$feature_id==131||$feature_id==193)
                         {
-                            $query="INSERT INTO goodshasfeature (goodshasfeature_valueint, goodshasfeature_valuefloat, ".
+							$query="INSERT INTO goodshasfeature (goodshasfeature_valueint, goodshasfeature_valuefloat, ".
                                 "goodshasfeature_valuetext, goods_id, feature_id) ".
                                 "VALUES ($goodshasfeature_valueint, $goodshasfeature_valuefloat, ".
                                 "'$goodshasfeature_valuetext', $mod_id, $feature_id)";
                             mysqli_query($db_connect,$query);
+							//echo "Удачно!<br>";
                             //echo $query."<br>";
                         }
                     }
@@ -277,7 +242,6 @@ class setFilters
         }
     }
 }
-
 /**
  * Class Timer
  */
@@ -314,7 +278,6 @@ class Timer
         return $this->end_time-$this->start_time;
     }
 }
-
 $runtime = new Timer();
 $runtime->setStartTime();
 $test=new setFilters();
