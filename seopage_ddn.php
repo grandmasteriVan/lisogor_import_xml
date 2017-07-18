28126da10836ce7ec7b177fffd371a157b7c226d
grandmasteriVan <grandmaster.ivan@gmail.com>
2017-07-18 10:54:18 +0300

    new

---
+++ seopage_ddn.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 18.07.17
+ * Time: 09:27
+ */
+header('Content-Type: text/html; charset=utf-8');
+/**
+ * database host
+ */
+define("host","localhost");
+//define ("host","localhost");
+/**
+ * database username
+ */
+define ("user", "root");
+//define ("user", "u_divani_n");
+/**
+ * database password
+ */
+define ("pass", "");
+//define ("pass", "EjcwKUYK");
+/**
+ * database name
+ */
+define ("db", "ddn_new");
+//define ("db", "divani_new");
+
+class SeoPage
+{
+    private function getGoodsId()
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $query="SELECT goods_id, goods_article FROM goods";
+        if ($res=mysqli_query($db_connect,$query))
+        {
+            while ($row = mysqli_fetch_assoc($res))
+            {
+                $goods[] = $row;
+            }
+            if (is_array($goods))
+            {
+                unset ($ids);
+                foreach ($goods as $good)
+                {
+                    //получаем нужный текст
+                    $ids[]=$good['goods_id'];
+                }
+            }
+        }
+        else
+        {
+            echo "Error in SQL: $query<br>";
+        }
+        mysqli_close($db_connect);
+        return $ids;
+    }
+
+    private function getSizeId($id)
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $query="SELECT goods_mainsize FROM goods WHERE goods_id=$id";
+        if ($res=mysqli_query($db_connect,$query))
+        {
+            while ($row = mysqli_fetch_assoc($res))
+            {
+                $sizes[] = $row;
+            }
+            if (is_array($sizes))
+            {
+                unset ($goodsSizeId);
+                foreach ($sizes as $size)
+                {
+                    $goodsSizeId=$size['goods_mainsize'];
+                }
+            }
+        }
+        else
+        {
+            echo "Error in SQL: $query<br>";
+        }
+        mysqli_close($db_connect);
+        return $goodsSizeId;
+    }
+
+    private function getMainSizeLen($id)
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $sizeId=$this->getSizeId($id);
+        $query="SELECT size_len FROM size WHERE size_id=$sizeId";
+        if ($res=mysqli_query($db_connect,$query))
+        {
+            while ($row = mysqli_fetch_assoc($res))
+            {
+                $sizes[] = $row;
+            }
+            if (is_array($sizes))
+            {
+                unset ($goodsLen);
+                foreach ($sizes as $size)
+                {
+                    $goodsLen=$size['goods_mainsize'];
+                }
+            }
+        }
+        else
+        {
+            echo "Error in SQL: $query<br>";
+        }
+        mysqli_close($db_connect);
+        return $goodsLen;
+
+    }
+
+    private function getMainSizeSl($id)
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $sizeId=$this->getSizeId($id);
+        $query="SELECT size_len FROM size WHERE size_id=$sizeId";
+        if ($res=mysqli_query($db_connect,$query))
+        {
+            while ($row = mysqli_fetch_assoc($res))
+            {
+                $sizes[] = $row;
+            }
+            if (is_array($sizes))
+            {
+                unset ($goodsLen);
+                foreach ($sizes as $size)
+                {
+                    $goodsLen=$size['goods_mainsize'];
+                }
+            }
+        }
+        else
+        {
+            echo "Error in SQL: $query<br>";
+        }
+        mysqli_close($db_connect);
+        return $goodsLen;
+    }
+
+    private function isCorner($id)
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $query="SELECT goods_id FROM goodshasfeature WHERE feature_id=14 AND goodshasfeature_valueid=56 AND goods_id=$id";
+        if ($res=mysqli_query($db_connect,$query))
+        {
+            while ($row = mysqli_fetch_assoc($res))
+            {
+                $corners[] = $row;
+            }
+            if (is_array($corners))
+            {
+                $isCorner=true;
+            }
+            else
+            {
+                $isCorner=false;
+            }
+        }
+        mysqli_close($db_connect);
+        return $isCorner;
+    }
+
+    public function setSmallCorner()
+    {
+        $all_div=$this->getGoodsId();
+        $smallCornerStr="";
+        foreach ($all_div as $div)
+        {
+            $id=$div['goods_id'];
+            $article=$div['goods_article'];
+            if ($this->isCorner($id))
+            {
+                $len=$this->getMainSizeLen($id);
+                if ($len<2200)
+                {
+                    $smallCornerStr.=", $article";
+                }
+            }
+        }
+        echo $smallCornerStr."<br>";
+    }
+}
+
+$test=new SeoPage();
+$test->setSmallCorner();
