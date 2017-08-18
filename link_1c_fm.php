ef66dd32a67b4a5baf33ee64665651dce9e587fa
grandmasteriVan <grandmaster.ivan@gmail.com>
2017-08-18 11:58:21 +0300

    Update link_1c_fm.php

--- link_1c_fm.php
+++ link_1c_fm.php
@@ -58,44 +58,93 @@ class Link
         {
             //var_dump($matches);
             $name=$matches[1];
-            $name=strtolower($name);
+            $name=mb_strtolower($name);
+			//$name=ucfirst($name);
+			$name=mb_convert_case($name,MB_CASE_TITLE);
         }
         else
         {
             echo "Not find name <br>";
         }
         //отсекаем первую двойную кавычку в тексте
-        $str=mb_substr($str,1);
-        if (preg_match("#\"(.+?)\;#is",$str,$matches))
+        $str1=mb_substr($str,2);
+        if (preg_match("#\"(.+?)\;#is",$str1,$matches))
         {
             //var_dump($matches);
             $sizes=$matches[1];
-            $sizes=explode("x",$sizes);
+            $sizes=explode("х",$sizes);
         }
         else
         {
             echo "Not find sizes <br>";
         }
         echo "Name=$name size_len=$sizes[0], size_width=$sizes[1] <br>";
-
+		$arr['name']=$name;
+		$arr['len']=$sizes[0];
+		$arr['width']=$sizes[1];
+		return $arr;
+		
     }
+	private function UTF8toCP1251($str)
+	{ // by SiMM, $table from http://ru.wikipedia.org/wiki/CP1251
+		static $table = array("\xD0\x81" => "\xA8", // Ё
+			"\xD1\x91" => "\xB8", // ё
+			// украинские символы
+			"\xD0\x8E" => "\xA1", // Ў (У)
+			"\xD1\x9E" => "\xA2", // ў (у)
+			"\xD0\x84" => "\xAA", // Є (Э)
+			"\xD0\x87" => "\xAF", // Ї (I..)
+			"\xD0\x86" => "\xB2", // I (I)
+			"\xD1\x96" => "\xB3", // i (i)
+			"\xD1\x94" => "\xBA", // є (э)
+			"\xD1\x97" => "\xBF", // ї (i..)
+			// чувашские символы
+			"\xD3\x90" => "\x8C", // &#1232; (А)
+			"\xD3\x96" => "\x8D", // &#1238; (Е)
+			"\xD2\xAA" => "\x8E", // &#1194; (С)
+			"\xD3\xB2" => "\x8F", // &#1266; (У)
+			"\xD3\x91" => "\x9C", // &#1233; (а)
+			"\xD3\x97" => "\x9D", // &#1239; (е)
+			"\xD2\xAB" => "\x9E", // &#1195; (с)
+			"\xD3\xB3" => "\x9F", // &#1267; (у)
+		);
+		//цифровая магия
+		$str = preg_replace('#([\xD0-\xD1])([\x80-\xBF])#se',
+			'isset($table["$0"]) ? $table["$0"] :
+							 chr(ord("$2")+("$1" == "\xD0" ? 0x30 : 0x70))
+							',
+			$str
+		);
+		$str = str_replace("i", "і", $str);
+		$str = str_replace("I", "І", $str);
+		return $str;
+	}
 	public function doLink($f_id)
 	{
-		//$db_connect=mysqli_connect(host,user,pass,db);
+		$db_connect=mysqli_connect(host,user,pass,db);
 		$this->ReadFile();
 		foreach ($this->data as $d)
 		{
 			
 			$code1c=$d[0];
 			$codePrice=$d[1];
-			$this->parceVelam($code1c);
+			$arr=$this->parceVelam($code1c);
+			$name=$arr['name'];
+			$len=$arr['len'];
+			$len.=0;
+			$width=$arr['width'];
+			$width.=0;
+			//$name=iconv("UTF-8","CP-1251",$name);
+			$name=$this->UTF8toCP1251($name);
+			$codePrice=$this->UTF8toCP1251($codePrice);
+			$query = "UPDATE goods SET goods_article_1c='$codePrice' WHERE goods_name like '%$name%' AND goods_length=$len AND goods_width=$width AND factory_id=$f_id";
 			//$query="UPDATE goods SET goods_article_1c='$code1c' WHERE goods_article_link='$codePrice' AND factory_id=$f_id";
-			//mysqli_query($db_connect,$query);
-            //echo $query."<br>";
+			mysqli_query($db_connect,$query);
+            echo $query."<br>";
+			//break;
 		}
-		mysqli_close($db_connect);
+		//mysqli_close($db_connect);
 	}
 }
-
 $test=new Link();
-$test->doLink(124);
+$test->doLink(137);
