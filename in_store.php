6b260bc5f3ed6a91525c17c49d0cf90009c57358
grandmasteriVan <grandmaster.ivan@gmail.com>
2017-08-31 15:35:39 +0300

    Update in_store.php

--- in_store.php
+++ in_store.php
@@ -5,33 +5,31 @@
  * Date: 04.04.17
  * Time: 09:56
  */
-
-header('Content-Type: text/html; charset=utf-8');
+//header('Content-Type: text/html; charset=utf-8');
 /**
  * database host
  */
-define ("host","localhost");
-define ("host_ddn","localhost");
+//define ("host","localhost");
+define ("host_ddn","es835db.mirohost.net");
 //define ("host","10.0.0.2");
 /**
  * database username
  */
-define ("user", "root");
-define ("user_ddn", "root");
+//define ("user", "root");
+define ("user_ddn", "u_fayni");
 //define ("user", "uh333660_mebli");
 /**
  * database password
  */
-define ("pass", "");
-define ("pass_ddn", "");
+//define ("pass", "");
+define ("pass_ddn", "ZID1c0eud3Dc");
 //define ("pass", "Z7A8JqUh");
 /**
  * database name
  */
-define ("db", "mebli");
-define ("db_ddn", "mebli");
+//define ("db", "mebli");
+define ("db_ddn", "ddnPZS");
 //define ("db", "uh333660_mebli");
-
 /**
  * Class InStoreFm
  */
@@ -45,7 +43,6 @@ class InStoreFm
      * @var
      */
     private $data;
-
     /**
      * InStoreFm constructor.
      */
@@ -54,7 +51,6 @@ class InStoreFm
         if ($f)
             $this->file=$f;
     }
-
     /**
      * @param $article
      * @param $in_store
@@ -63,15 +59,14 @@ class InStoreFm
     {
         $this->data[]=array('article'=>$article, 'in_store'=>$in_store);
     }
-
     /**
      *
      */
-    public function readFile()
+    public function readFile1()
     {
         if ($this->file)
         {
-            $dom=DOMDocument::load($this->file1);
+            $dom=DOMDocument::load($this->file);
             $rows=$dom->getElementsByTagName('Row');
             $row_num=1;
             foreach ($rows as $row)
@@ -97,13 +92,15 @@ class InStoreFm
                     {
                         $this->add_data($article,$in_store);
                     }
-
                 }
                 $row_num++;
             }
         }
+		else
+		{
+			echo "no file in InStoreFm-readFile";
+		}
     }
-
     /**
      *удаляет все отметки на складе для мягкой мебели
      */
@@ -121,20 +118,22 @@ class InStoreFm
         }
         mysqli_close($db_connect);
     }
-
     /**
      *
      */
     public function setInSore()
     {
         $db_connect=mysqli_connect(host,user,pass,db);
+		//var_dump($this->file);
+		$this->readFile1();
         if ($this->data)
         {
             foreach ($this->data as $d)
             {
                 $article=$d['article'];
                 $in_store=$d['in_store'];
-                $query="UPDATE goods SET goods_avail=$in_store WHERE goods_article=$article";
+                $query="UPDATE goods SET goods_avail=$in_store WHERE goods_article='$article'";
+				//echo "$query<br>";
                 if (mysqli_query($db_connect,$query))
                 {
                     echo "In Store set for $article<br>";
@@ -145,15 +144,17 @@ class InStoreFm
                     echo "Error ".mysqli_error($db_connect)."<br>";
                     echo "$query<br>";
                 }
-
             }
+			echo "<b>end FM</b><br>";
         }
+		else
+		{
+			echo"no data<br>";
+		}
         mysqli_close($db_connect);
     }
-
 }
-
-class SetInStoreDDN
+class InStoreDDN
 {
     /**
      * @var
@@ -163,20 +164,27 @@ class SetInStoreDDN
      * @var
      */
     private $data;
-
     /**
      * InStoreFm constructor.
      */
     public function __construct($f)
     {
         if ($f)
-            $this->file=$f;
+            $this->file1=$f;
     }
-
+	/**
+     * @param $article
+     * @param $in_store
+     */
+    public function add_data($article, $in_store)
+    {
+        $this->data[]=array('article'=>$article, 'in_store'=>$in_store);
+    }
     private function getIdByArticle($article)
     {
         $db_connect=mysqli_connect(host_ddn,user_ddn,pass_ddn,db_ddn);
-        $query="SELECT goods_id FROM goods WHERE goods_article=$article";
+        $query="SELECT goods_id FROM goods WHERE goods_article='$article'";
+		//echo "$query<br>";
         if ($res=mysqli_query($db_connect,$query))
         {
             while ($row = mysqli_fetch_assoc($res))
@@ -191,6 +199,10 @@ class SetInStoreDDN
                     $goods_id=$id['goods_id'];
                 }
             }
+			else
+			{
+				echo "no SQL Data<br>";
+			}
         }
         else
         {
@@ -200,10 +212,10 @@ class SetInStoreDDN
         mysqli_close($db_connect);
         return $goods_id;
     }
-
-    public function readFile()
+    public function readFile1()
     {
-        if ($this->file)
+        //var_dump ($this->file1);
+		if ($this->file1)
         {
             $dom=DOMDocument::load($this->file1);
             $rows=$dom->getElementsByTagName('Row');
@@ -217,7 +229,7 @@ class SetInStoreDDN
                     foreach ($cells as $cell)
                     {
                         $elem=$cell->nodeValue;
-                        if ($cell_num==3)
+                        if ($cell_num==4)
                         {
                             $article=$elem;
                         }
@@ -231,17 +243,22 @@ class SetInStoreDDN
                     {
                         $this->add_data($article,$in_store);
                     }
-
                 }
                 $row_num++;
             }
         }
+		else
+		{
+			echo "no file in DDN<br>";
+		}
     }
-
     public function setInSore()
     {
-        $db_connect=mysqli_connect(host_ddn,user_ddn,pass_ddn,db_ddn);
-        if ($this->data)
+        echo "begin DDN<br>";
+		$db_connect=mysqli_connect(host_ddn,user_ddn,pass_ddn,db_ddn);
+        $this->readFile1();
+		//var_dump ($this->data);
+		if ($this->data)
         {
             foreach ($this->data as $d)
             {
@@ -250,13 +267,14 @@ class SetInStoreDDN
                 $in_store=$d['in_store'];
                 if ($in_store==0)
                 {
-                    $in_store=1;
+                    $in_store=2;
                 }
                 else
                 {
-                    $in_store=2;
+                    $in_store=1;
                 }
                 $query="UPDATE goodshasfeature SET goodshasfeature_valueid=$in_store WHERE goods_id=$id AND feature_id=16";
+				//echo "$query<br>";
                 if (mysqli_query($db_connect,$query))
                 {
                     echo "In Store set for $article<br>";
@@ -267,9 +285,13 @@ class SetInStoreDDN
                     echo "Error ".mysqli_error($db_connect)."<br>";
                     echo "$query<br>";
                 }
-
+				//break;
             }
         }
+		else 
+		{
+			echo "no data in DDN";
+		}
         mysqli_close($db_connect);
     }
 }
