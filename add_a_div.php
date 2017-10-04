1313dffa7d2be5ece25452d2f3f10c2dd37d7b01
grandmasteriVan <grandmaster.ivan@gmail.com>
2017-10-04 16:17:58 +0300

    Update add_a_div.php

--- add_a_div.php
+++ add_a_div.php
@@ -5,15 +5,17 @@
  * Date: 04.10.2017
  * Time: 14:32
  */
-define ("host","es835db.mirohost.net");
-define ("user", "u_fayni");
-define ("pass", "ZID1c0eud3Dc");
+define ("host","localhost");
+define ("user", "u_ddnPZS");
+define ("pass", "A8mnsoHf");
 define ("db", "ddnPZS");
-
+//define ("host","localhost");
+//define ("user", "root");
+//define ("pass", "");
+//define ("db", "ddn_new");
 class MakeActive
 {
     private $f_id;
-
     /**
      * MakeActive constructor.
      * @param $f_id
@@ -22,8 +24,6 @@ class MakeActive
     {
         $this->f_id = $f_id;
     }
-
-
     /**
      * выбираем список айди товаров с сайта ДДН по определенной фабрике
      * @param $f_id integer - айди фабрики на ДДН
@@ -31,7 +31,7 @@ class MakeActive
      */
     private function getGoodsByFactory ($f_id)
     {
-        $db_connect=mysqli_connect(host_ddn,user_ddn,pass_ddn,db_ddn);
+        $db_connect=mysqli_connect(host,user,pass,db);
         $query="SELECT DISTINCT goods.goods_id FROM goods join goodshasfeature on goods.goods_id=goodshasfeature.goods_id WHERE goodshasfeature.goods_id in (select goodshasfeature.goods_id from goodshasfeature where feature_id=14 AND goodshasfeature_valueid=$f_id)";
         if ($res=mysqli_query($db_connect,$query))
         {
@@ -40,7 +40,6 @@ class MakeActive
             {
                 $idByFactoty[]=$row;
             }
-
         }
         else
         {
@@ -56,16 +55,15 @@ class MakeActive
             return null;
         }
     }
-
     /**
      * проверяем есть ли запись для данной категории
      * @param $id - айди категории
      * @return bool true - если запись есть, false - если нет
      */
-    private function checkDuplicate($id)
+    private function checkDuplicate($id,$t_id)
     {
         $db_connect=mysqli_connect(host,user,pass,db);
-        $query="SELECT goodshastissue_id FROM goodshastissue_id WHERE tissue_id=$id";
+        $query="SELECT goodshastissue_id FROM goodshastissue WHERE goods_id=$id AND tissue_id=$t_id";
         //echo $query."<br>";
         if ($res=mysqli_query($db_connect,$query))
         {
@@ -82,6 +80,10 @@ class MakeActive
                 }
             }
         }
+		else
+		{
+			echo "error in SQL $query<br>";
+		}
         //mysqli_query($db_connect, $query);
         //var_dump ($art);
         if ($art==null)
@@ -97,21 +99,28 @@ class MakeActive
     {
         $db_connect=mysqli_connect(host,user,pass,db);
         $all_divs=$this->getGoodsByFactory($this->f_id);
+		//var_dump($all_divs);
         foreach ($all_divs as $div)
         {
-            if ($this->checkDuplicate($div))
+            //var_dump($div);
+			$div=$div['goods_id'];
+			if (!$this->checkDuplicate($div,$issue_id))
             {
                 $query="INSERT INTO goodshastissue (goodshastissue_price, goodshastissue_pricecur, goodshastissue_active, goods_id,tissue_id)".
                     " VALUES (0,0,1,$div,$issue_id)";
                 mysqli_query($db_connect, $query);
                 echo "$query<br>";
             }
+			else
+			{
+				echo "Find Cat $issue_id in good $div!<br>";
+			}
         }
     }
     private function getGoodsHasTissueId($goods_id, $tissue_id)
     {
         $db_connect=mysqli_connect(host,user,pass,db);
-        $query="SELECT goodshastissue_id FROM goodshastissue_id WHERE tissue_id=$tissue_id AND goods_id=$goods_id";
+        $query="SELECT goodshastissue_id FROM goodshastissue WHERE tissue_id=$tissue_id AND goods_id=$goods_id";
         //echo $query."<br>";
         if ($res=mysqli_query($db_connect,$query))
         {
@@ -128,6 +137,10 @@ class MakeActive
                 }
             }
         }
+		else
+		{
+			echo "Error in SQL $query<br>";
+		}
         mysqli_close($db_connect);
         if ($art!=null)
         {
@@ -146,7 +159,6 @@ class MakeActive
         echo "$query<br>";
         mysqli_close($db_connect);
     }
-
     public function makeA($issue_id)
     {
         //добавляем в каждый диван нужную категорию
@@ -154,11 +166,12 @@ class MakeActive
         $all_div=$this->getGoodsByFactory($this->f_id);
         foreach ($all_div as $div)
         {
-            $goods_maincategory=$this->getGoodsHasTissueId($div,$issue_id);
+            $div=$div['goods_id'];
+			$goods_maincategory=$this->getGoodsHasTissueId($div,$issue_id);
+			//var_dump($goods_maincategory);
             $this->setGoodsMainCat($div,$goods_maincategory);
         }
     }
 }
-
 $test=new MakeActive(82);
 $test->makeA(299);
