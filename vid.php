9fb8b843b17fbf76aa39f107e6bab8285a0f6539
grandmasteriVan <grandmaster.ivan@gmail.com>
2018-03-12 12:26:37 +0200

    Update vid.php

--- vid.php
+++ vid.php
@@ -192,7 +192,7 @@ class insertVidBeds
     private function getTovNoVid()
     {
         $db_connect=mysqli_connect(host,user,pass,db);
-        $query="SELECT goods_id, goods_content FROM goods WHERE goods_active=1 AND goods_noactual=0 AND goods_content NOT LIKE '%iframe%' AND (goodskind_id=39 OR goodskind_id=50 ORgoodskind_id=121)";
+        $query="SELECT goods_id, goods_content FROM goods WHERE goods_active=1 AND goods_noactual=0 AND goods_content NOT LIKE '%iframe%' AND (goodskind_id=39 OR goodskind_id=50 OR goodskind_id=121)";
         if ($res=mysqli_query($db_connect,$query))
         {
             while ($row = mysqli_fetch_assoc($res))
@@ -205,7 +205,7 @@ class insertVidBeds
             echo "error in SQL: $query<br>";
         }
         mysqli_close($db_connect);
-        if (!is_array($goods))
+        if (is_array($goods))
         {
             return $goods;
         }
@@ -214,23 +214,19 @@ class insertVidBeds
             return null;
         }
     }
-
     private function insertSingleVid($id, $cont)
     {
         $db_connect=mysqli_connect(host,user,pass,db);
-        $cont="<p><iframe allow=\"encrypted-media\" allowfullscreen=\"\" frameborder=\"0\" gesture=\"media\" height=\"214\" src=\"https://www.youtube.com/embed/EoGsmck1bZI\" style=\"text-align: center;\" width=\"380\"></iframe></p>".$cont;
+        $cont="<p style=\"text-align: center;\"><iframe allow=\"encrypted-media\" allowfullscreen=\"\" frameborder=\"0\" gesture=\"media\" height=\"214\" src=\"https://www.youtube.com/embed/EoGsmck1bZI\" style=\"text-align: center;\" width=\"380\"></iframe></p>".$cont;
         $query="UPDATE goods SET goods_content='$cont' WHERE goods_id=$id";
-        //mysqli_query($db_connect,$query);
-        echo "$query <br>";
+        mysqli_query($db_connect,$query);
+        //echo "$query <br>";
         mysqli_close($db_connect);
     }
-
-
-
     private function getTovFirst()
     {
         $db_connect=mysqli_connect(host,user,pass,db);
-        $query="SELECT goods_id, goods_content FROM goods WHERE goods_active=1 AND goods_noactual=0 AND (goodskind_id=39 OR goodskind_id=50 ORgoodskind_id=121) AND factory_id<>32";
+        $query="SELECT goods_id, goods_content FROM goods WHERE goods_active=1 AND goods_noactual=0 AND (goodskind_id=39 OR goodskind_id=50 OR goodskind_id=121) AND factory_id<>32";
         if ($res=mysqli_query($db_connect,$query))
         {
             while ($row = mysqli_fetch_assoc($res))
@@ -307,10 +303,44 @@ class insertVidBeds
 			echo "No last array!!<br>";
 		}
 	}
-
+	
+	public function getNoVid()
+	{
+		$db_connect=mysqli_connect(host,user,pass,db);
+        $query="SELECT goods_id FROM goods WHERE goods_active=1 AND goods_noactual=0 AND (goodskind_id=39 OR goodskind_id=50 OR goodskind_id=121) AND goods_content NOT LIKE '%EoGsmck1bZI%'";
+        if ($res=mysqli_query($db_connect,$query))
+        {
+            while ($row = mysqli_fetch_assoc($res))
+            {
+                $goods[] = $row;
+            }
+        }
+        else
+        {
+            echo "error in SQL: $query<br>";
+        }
+        mysqli_close($db_connect);
+		//var_dump
+		if (is_array($goods))
+		{
+			foreach ($goods as $good)
+			{
+				$id=$good['goods_id'];
+				echo "$id<br>";
+			}
+		}
+		else
+		{
+			echo "No array!";
+		}
+        
+	}
+	
 	public function goSingle()
     {
         $goods=$this->getTovNoVid();
+		//var_dump($goods);
+		
         if (is_array($goods))
         {
             foreach ($goods as $good)
@@ -329,6 +359,8 @@ class insertVidBeds
 $runtime = new Timer();
 $runtime->setStartTime();
 $test=new insertVidBeds();
+$test->getNoVid();
+//$test->goSingle();
 //$test->goLast();
 //$test=new CopyVid();
 //$test->FindVideo();
