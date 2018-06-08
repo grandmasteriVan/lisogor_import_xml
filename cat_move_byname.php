79d9e6c7660e4c1dbec6ebd598343a123094aba9
grandmasteriVan <grandmaster.ivan@gmail.com>
2018-06-08 09:45:57 +0300

    Update cat_move_byname.php

--- cat_move_byname.php
+++ cat_move_byname.php
@@ -5,7 +5,6 @@
  * Date: 07.06.2018
  * Time: 16:20
  */
-
 header('Content-type: text/html; charset=UTF-8');
 //define ("host","localhost");
 define ("host","localhost");
@@ -24,7 +23,6 @@ define ("pass", "T6n7C8r1");
  */
 //define ("db", "mebli");
 define ("db", "fm");
-
 class CatMove
 {
     private function getGoods()
@@ -49,10 +47,8 @@ class CatMove
         }
         return 0;
     }
-
     public function moveCat()
     {
-
         $db_connect=mysqli_connect(host,user,pass,db);
         $goods=$this->getGoods();
         if (is_array($goods))
@@ -61,38 +57,44 @@ class CatMove
             {
                 $id=$good['goods_id'];
                 $name=" ".$good['goods_name'];
-                if (mb_strpos($name, "Пенал")||mb_strpos($name, "пенал")||mb_strpos($name, "Стеллаж")||mb_strpos($name, "стеллаж")||mb_strpos($name, "Витрина")||mb_strpos($name, "витрина")||mb_strpos($name, "Угловой элемент")||mb_strpos($name, "угловой элемент")||mb_strpos($name, "Сервант")||mb_strpos($name, "сервант"))
+                if ((mb_strpos($name, "Пенал")||mb_strpos($name, "пенал")||mb_strpos($name, "Стеллаж")||mb_strpos($name, "стеллаж")||mb_strpos($name, "Витрина")||mb_strpos($name, "витрина")||mb_strpos($name, "Угловой элемент")||mb_strpos($name, "угловой элемент")||mb_strpos($name, "Сервант")||mb_strpos($name, "сервант"))&&(!mb_strpos($name, "Кухня")))
                 {
-                    $query="DELETE FROM goodshastcharter WHERE goods_id=$id";
-                    //mysqli_query($db_connect,$query);
+                    echo "$name<br>";
+					$query="DELETE FROM goodshastcharter WHERE goods_id=$id";
+                    mysqli_query($db_connect,$query);
                     echo $query."<br>";
                     $query="INSERT INTO goodshastcharter (goods_id, tcharter_id) VALUES ($id,10)";
-                    //mysqli_query($db_connect,$query);
+                    mysqli_query($db_connect,$query);
+					echo $query."<br>";
                     $query="UPDATE goods SET goods_maintcharter=10 WHERE goods_id=$id";
-                    //mysqli_query($db_connect,$query);
-                    //echo $i.". ".$query."<br>";
+                    mysqli_query($db_connect,$query);
+                    echo $query."<br>";
                 }
                 if (mb_strpos($name, "Полка")||mb_strpos($name, "полка")||mb_strpos($name, "Надствка")||mb_strpos($name, "надставка"))
                 {
-                    $query="DELETE FROM goodshastcharter WHERE goods_id=$id";
-                    //mysqli_query($db_connect,$query);
+                    echo "$name<br>";
+					$query="DELETE FROM goodshastcharter WHERE goods_id=$id";
+                    mysqli_query($db_connect,$query);
                     echo $query."<br>";
                     $query="INSERT INTO goodshastcharter (goods_id, tcharter_id) VALUES ($id,11)";
-                    //mysqli_query($db_connect,$query);
+                    mysqli_query($db_connect,$query);
+					echo $query."<br>";
                     $query="UPDATE goods SET goods_maintcharter=11 WHERE goods_id=$id";
-                    //mysqli_query($db_connect,$query);
-                    //echo $i.". ".$query."<br>";
+                    mysqli_query($db_connect,$query);
+                    echo $query."<br>";
                 }
-                if (mb_strpos($name, "Бар")||mb_strpos($name, "бар"))
+                if (mb_strpos($name, "Бар ")||mb_strpos($name, "бар "))
                 {
-                    $query="DELETE FROM goodshastcharter WHERE goods_id=$id";
-                    //mysqli_query($db_connect,$query);
+                    echo "$name<br>";
+					$query="DELETE FROM goodshastcharter WHERE goods_id=$id";
+                    mysqli_query($db_connect,$query);
                     echo $query."<br>";
                     $query="INSERT INTO goodshastcharter (goods_id, tcharter_id) VALUES ($id,12)";
-                    //mysqli_query($db_connect,$query);
+                    mysqli_query($db_connect,$query);
+					echo $query."<br>";
                     $query="UPDATE goods SET goods_maintcharter=12 WHERE goods_id=$id";
-                    //mysqli_query($db_connect,$query);
-                    //echo $i.". ".$query."<br>";
+                    mysqli_query($db_connect,$query);
+                    echo $query."<br>";
                 }
             }
         }
@@ -103,3 +105,6 @@ class CatMove
         mysqli_close($db_connect);
     }
 }
+
+$test=new CatMove();
+$test->moveCat();
