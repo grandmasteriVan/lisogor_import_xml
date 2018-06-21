2c5e731261a9e150e4b067ff7be7431b2bbf3261
grandmasteriVan <grandmaster.ivan@gmail.com>
2018-06-21 11:15:58 +0300

    Update corners_rep.php

--- reports/corners_rep.php
+++ reports/corners_rep.php
@@ -5,10 +5,27 @@
  * Date: 21.06.2018
  * Time: 10:08
  */
-
+ 
+header('Content-type: text/html; charset=UTF-8');
+//define ("host","localhost");
+define ("host","localhost");
+/**
+ * database username
+ */
+//define ("user", "root");
+define ("user", "fm");
+/**
+ * database password
+ */
+//define ("pass", "");
+define ("pass", "T6n7C8r1");
+/**
+ * database name
+ */
+//define ("db", "mebli");
+define ("db", "fm");
 class Corners
 {
-
     private function getGoods()
     {
         $db_connect=mysqli_connect(host,user,pass,db);
@@ -34,29 +51,28 @@ class Corners
             return 0;
         }
     }
-
     private function setActive($id)
     {
         $db_connect=mysqli_connect(host,user,pass,db);
         $query="update goods SET goods_active=1 where goods_id=$id";
         mysqli_query($db_connect,$query);
+		//echo "$query<br>";
         mysqli_close($db_connect);
     }
-
     private function addVid($id,$cont,$charter)
     {
-        $add="<p style=\"text-align: center;\"><iframe allow=\"encrypted-media\" allowfullscreen=\"\" frameborder=\"0\" gesture=\"media\" height=\"214\" src=\"https://www.youtube.com/embed/EoGsmck1bZl\" style=\"text-align: center;\" width=\"380\"></iframe></p>";
+        $add="<p style=\"text-align: center;\"><iframe allow=\"encrypted-media\" allowfullscreen=\"\" frameborder=\"0\" gesture=\"media\" height=\"214\" src=\"https://www.youtube.com/embed/EoGsmck1bZI\" style=\"text-align: center;\" width=\"380\"></iframe></p>";
 		if ($charter==33)
         {
-            $add.="<p>Цена указана за кровать без матраса и подъемного механизма. За дополнительной информацией обращайтесь к менеджерам нашегг магазина</p>";
+            $add.="<p>Цена указана за кровать без матраса и подъемного механизма. За дополнительной информацией обращайтесь к менеджерам нашего магазина</p>";
         }
         $cont=$add.$cont;
         $db_connect=mysqli_connect(host,user,pass,db);
         $query="update goods SET goods_content='$cont' where goods_id=$id";
         mysqli_query($db_connect,$query);
+		//echo "$query<br>";
         mysqli_close($db_connect);
     }
-
     public function checkCorners()
     {
         $goods=$this->getGoods();
@@ -67,10 +83,9 @@ class Corners
                 $id=$good['goods_id'];
                 $charter=$good['goods_maintcharter'];
                 $cont=$good['goods_content'];
+				//$cont="";
                 $this->setActive($id);
                 $this->addVid($id,$cont,$charter);
-
-
             }
         }
         else
@@ -79,6 +94,5 @@ class Corners
         }
     }
 }
-
 $test=new Corners();
 $test->checkCorners();
