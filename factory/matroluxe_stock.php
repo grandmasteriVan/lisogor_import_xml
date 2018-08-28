b8e94a4647773fea95c83b0c452d65164a18bcec
grandmasteriVan <grandmaster.ivan@gmail.com>
2018-08-28 14:47:43 +0300

    Update matroluxe_stock.php

--- factory/matroluxe_stock.php
+++ factory/matroluxe_stock.php
@@ -147,9 +147,9 @@ class Matroluxe
     public function setStock($f_id)
     {
         $price1c=$this->readFile("matroluxe.txt");
-		//echo "<pre>";
-		//print_r($price1c);
-		//echo "</pre>";
+		echo "<pre>";
+		print_r($price1c);
+		echo "</pre>";
         if (is_array($price1c))
         {
             foreach ($price1c as $pos1c)
@@ -226,8 +226,91 @@ class Asat extends Matroluxe
     }
 }
 
+class Corners extends Matroluxe
+{
+	public function getGood($article,$f_id)
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $query="SELECT goods_id FROM goods WHERE goods_article_1c='$article' AND factory_id=$f_id";
+        if ($res=mysqli_query($db_connect,$query))
+        {
+            while ($row = mysqli_fetch_assoc($res))
+            {
+                $good[] = $row;
+            }
+        }
+        else
+        {
+            echo "Error in SQL getGood ".mysqli_error($db_connect)."<br>";
+        }
+        mysqli_close($db_connect);
+        return $good;
+    }
+	
+	public function setStock($f_id)
+    {
+        $price1c=$this->readFile("corners.txt");
+		$this->unsetStock($f_id);
+		//echo "<pre>";
+		//print_r($price1c);
+		//echo "</pre>";
+        if (is_array($price1c))
+        {
+            foreach ($price1c as $pos1c)
+            {
+                
+				$article=$pos1c[2]."/".$pos1c[3];
+				echo "$article<br>";
+                $goods=$this->getGood($article,$f_id);
+				var_dump ($goods);
+				//$goods=$this->getGood();
+				if (is_array ($goods))
+				{
+					foreach ($goods as $good)
+					{
+						$good_id=$good['goods_id'];
+						//var_dump($good);
+						//echo "$good_id<br>";
+						$old_price=$this->getOldPrice($good_id);
+						$new_price=$pos1c[6];
+						//$good_id=$good['goods_id'];
+						$discont=$this->getPercent($old_price,$new_price);
+						echo "<pre>";
+						print_r($pos1c);
+						echo "</pre>";
+						echo "goods_id=$good_id old_price=$old_price, new_price=$new_price, discont=$discont<br>";
+						if ($discont>1&&$discont<99)
+						{
+							$this->writePrice($old_price,$new_price,$discont,$good_id);
+						}
+						//else
+						//{
+						//	$old_price=round($new_price*1.15);
+						//	$discont=$this->getPercent($old_price,$new_price);
+						//	$this->writePrice($old_price,$new_price,$discont,$good_id);
+						//}
+						//break;
+					}
+				}
+				else
+				{
+					echo "No goods!";
+				}
+                
+            }
+        }
+        else
+        {
+            echo "No price!";
+        }
+    }
+}
+
 
 //$test=new Matroluxe();
 //$test->setStock(46);
-$test=new Asat();
-$test->setStock(197);
+//$test=new Asat();
+//$test->setStock(197);
+$test=new Corners();
+$test->setStock(202);
+
