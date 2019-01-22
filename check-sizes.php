46101aca8cb1d9a27f11ef296240bc4368a06678
grandmasteriVan <grandmaster.ivan@gmail.com>
2019-01-22 11:33:24 +0200

    Add files via upload

---
+++ check-sizes.php
@@ -0,0 +1,150 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+/**
+ * database host
+ */
+//define ("host","localhost");
+define ("host","localhost");
+/**
+ * database username
+ */
+//define ("user", "root");
+define ("user", "newfm");
+/**
+ * database password
+ */
+//define ("pass", "");
+define ("pass", "N0r7F8g6");
+/**
+ * database name
+ */
+//define ("db", "fm_new");
+define ("db", "newfm");
+
+class checkSizes
+{
+    
+    private $cat_id = 1;
+
+    public $need_string = 'ff';
+    
+    function __construct()
+    {
+        //var_dump ($_GET);
+        $sizes=$_GET['sizes'];
+        $cat_id=$_GET["cat_id"];
+        //echo gettype($this->need_string);
+       //echo "$sizes $cat_id";
+        $this->cat_id=$cat_id;
+        $this->need_string=$sizes;
+        //$this->$cat_id=$_GET["cat_id"];
+        //$this->$need_string=$_GET['sizes'];
+    }
+
+    
+    
+    private function getSizes($id)
+    {
+       
+		$db_connect=mysqli_connect(host,user,pass,db);
+		$query="select goods_length, goods_width, goods_height, goods_depth from goods WHERE goods_id=$id";
+		if ($res=mysqli_query($db_connect,$query))
+		{
+				while ($row = mysqli_fetch_assoc($res))
+				{
+					$sizes[] = $row;
+				}
+		}
+		else
+		{
+			 echo "Error in SQL: $query<br>";		
+		}
+		mysqli_close($db_connect);
+		if (is_array($sizes))
+		{
+			return $sizes;
+		}
+		else
+		{
+			return null;
+		}
+	
+    }
+
+    private function getGoodsByCategory($cat_id)
+	{
+		$db_connect=mysqli_connect(host,user,pass,db);
+		$query="select goods_id from goodshascategory WHERE category_id=$cat_id";
+		if ($res=mysqli_query($db_connect,$query))
+		{
+				while ($row = mysqli_fetch_assoc($res))
+				{
+					$goods_all[] = $row;
+				}
+		}
+		else
+		{
+			 echo "Error in SQL: $query<br>";		
+		}
+		return $goods_all;
+    }
+    
+    public function test()
+    {
+        $goods=$this->getGoodsByCategory($this->cat_id);
+        if (is_array($goods))
+        {
+            foreach ($goods as $good)
+            {
+                $id=$good['goods_id'];
+                $sizes=$this->getSizes($id);
+                $size_string="";
+                //var_dump ($sizes[0]['goods_width']);
+                if (strrpos($this->need_string,"width"))
+                {
+                    if (!($sizes[0]['goods_width']>0))
+                    {
+                        $size_string.="нет ширины ";
+                    }
+                }
+                if (strrpos($this->need_string,"length"))
+                {
+                    if (!($sizes[0]['goods_height']>0))
+                    {
+                        $size_string.="нет длины  ";
+                    }
+                }
+                if (strrpos($this->need_string,"depth"))
+                {
+                    if (!($sizes[0]['goods_depth']>0))
+                    {
+                        $size_string.="нет глубины  ";
+                    }
+                }
+                if (strrpos($this->need_string,"height"))
+                {
+                    if (!($sizes[0]['goods_height']>0))
+                    {
+                        $size_string.="нет высоты  ";
+                    }
+                }
+                //var_dump($sizes);
+                //echo"<br><br>";
+                //echo "$size_string";
+               // break;
+
+                if ($size_string!="")
+                {
+                    $size_string="у товара с ид=$id ".$size_string;
+                    echo $size_string."<br>";
+                }
+            }
+        }
+    }
+
+}
+
+$test = new checkSizes();
+echo "шкафы распашные:<br>";
+$test->test();
+//https://fayni-mebli.com/check-sizes.php?cat_id=10&sizes=width,height,depth
