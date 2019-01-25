5534235cd453b51fc41dce44f29cb9fede32ce15
grandmasteriVan <grandmaster.ivan@gmail.com>
2019-01-25 11:48:14 +0200

    Add files via upload

--- n.php
+++ n.php
@@ -2544,6 +2544,7 @@ if ($res=mysqli_query($db_connect,$query))
 
 	
 	///////////////////////////////////////////////////////////////
+	/*
 	function test()
 	{
 		$goods=getGoodsByCategory(14);
@@ -2922,7 +2923,100 @@ if ($res=mysqli_query($db_connect,$query))
 
 	//copyFiltersMatr(14);
 	test()
-	
+	*/
+
+	function getFeatures($good_id)
+	{
+		$db_connect=mysqli_connect(host,user,pass,db);
+		$query="select goodshasfeature_valueid, feature_id from goodshasfeature WHERE goods_id=$good_id";
+		if ($res=mysqli_query($db_connect,$query))
+		{
+				while ($row = mysqli_fetch_assoc($res))
+				{
+					$goods[] = $row;
+				}
+		}
+		else
+		{
+			 echo "Error in SQL: $query<br>";		
+		}
+		mysqli_close($db_connect);
+		if (is_array($goods))
+		{
+			return $goods;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	function getGoodsByCatAndFactory($cat_id, $f_id)
+	{
+		$db_connect=mysqli_connect(host,user,pass,db);
+		$query="select goods_id from goodshascategory WHERE category_id=$cat_id";
+		if ($res=mysqli_query($db_connect,$query))
+		{
+				while ($row = mysqli_fetch_assoc($res))
+				{
+					$goods_all[] = $row;
+				}
+		}
+		else
+		{
+			 echo "Error in SQL: $query<br>";		
+		}
+		if (is_array ($goods_all))
+		{
+			//var_dump($goods_all);
+			foreach ($goods_all as $good)
+			{
+				$id=$good['goods_id'];
+				$features=getFeatures($id);
+				if (is_array($features))
+				{
+					foreach ($features as $feature)
+					{
+						$feature_id=$feature['feature_id'];
+						$val_id=$feature['goodshasfeature_valueid'];
+						if ($feature_id==232&&$val_id==$f_id)
+						{
+							$goods_by_factoty[]=$id;
+							break;
+						}
+					}
+				}
+				
+				//break;
+			}
+		}
+		else
+		{
+			echo "no goods by category<br>";
+		}
+		
+		mysqli_close($db_connect);
+		if (is_array($goods_by_factoty))
+		{
+			return $goods_by_factoty;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	$db_connect=mysqli_connect(host,user,pass,db);
+	$goods=getGoodsByCatAndFactory(148,86);
+	foreach ($goods as $good)
+	{
+		$id=$good;
+		$query="UPDATE goodshaslang SET goodshaslang_active=1 WHERE goods_id=$id";
+		echo "$query<br>";
+		mysqli_query($db_connect,$query);
+
+	}
+	mysqli_close($db_connect);
 
 ?>
 
