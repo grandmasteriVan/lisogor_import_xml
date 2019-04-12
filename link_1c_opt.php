253b4e92d62ea3c443189a42c118b82e5d9f613c
grandmasteriVan <grandmaster.ivan@gmail.com>
2019-04-12 14:58:46 +0300

    Add files via upload

---
+++ link_1c_opt.php
@@ -0,0 +1,127 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+/**
+ * database host
+ */
+//define ("host","localhost");
+/**
+ *
+ */
+define ("host","localhost");
+/**
+ * database username
+ */
+//define ("user", "root");
+define ("user", "optmebli");
+/**
+ * database password
+ */
+//define ("pass", "");
+define ("pass", "VRYA1Q0R");
+/**
+ * database name
+ */
+//define ("db", "mebli");
+define ("db", "optmebli");
+
+class Link1C
+{
+    public $data;
+    /**
+     *
+     */
+    public $filename;
+    /**
+     * Link constructor.
+     * @param $filename
+     */
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+    private function ReadFile()
+    {
+        $handle=fopen($this->filename,"r");
+        while (!feof($handle))
+        {
+            $str=fgets($handle);
+			$str=explode(";",$str);
+			//для парсинга Велам, закоментить при обычном файлке!
+			//$str[0].=";";
+			$arr[]=$str;
+            //echo "$str<br>";
+        }
+        if (!empty($arr))
+        {
+            $this->data = $arr;
+        }
+        else
+        {
+            echo "array is empty in ReadFile";
+        }
+    }
+
+    public function parseGarant()
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $this->ReadFile();
+        foreach ($this->data as $d)
+        {
+            $code1c=$d[1];
+            $name=$d[0];
+            $name="Шкаф-купе".$name;
+
+            $query = "UPDATE goods join goodshaslang on goods.goods_id=goodshaslang.goods_id SET goods.goods_article_1c='$code1c' WHERE goodshaslang.goodshaslang_name like '$name' and goodshaslang.lang_id=1";
+            mysqli_query($db_connect,$query);
+            echo "$query<br>";
+        }
+    }
+}
+
+class SetFilters
+{
+    private function getGoods()
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $query="SELECT goods_id, goodshaslang_name from goodshaslang WHERE lang_id=1 AND goodshaslang_active=0 AND goodshaslang_name like '%Шкаф-купе%'";
+        if ($res=mysqli_query($db_connect,$query))
+        {
+                while ($row = mysqli_fetch_assoc($res))
+                {
+                    $goods[] = $row;
+                }
+        }
+        else
+        {
+            echo "Error in SQL: $query<br>";		
+        }
+        mysqli_close($db_connect);
+    }
+
+    private function delFeature($goods_id, $feature_id)
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $query="DELETE FROM goodshasfeature WHERE goods_id=$goods_id AND feature_id=$feature_id";
+        echo "$query<br>";
+        mysqli_query($db_connect,$query);
+        mysqli_close($db_connect);
+    }
+
+    function insFilter($goods_id, $feature_id, $value_id)
+    {
+        $db_connect=mysqli_connect(host,user,pass,db);
+        $query="INSERT INTO goodshasfeature (goods_id, feature_id, goodshasfeature_valueid) VALUES ($goods_id, $feature_id, $value_id)";
+        echo "$query<br><br>";
+        mysqli_query($db_connect,$query);
+        mysqli_close($db_connect);
+    }
+
+    public function test ()
+    {
+        $goods=$this->getGoods();
+        
+    }
+}
+
+//$test=new Link1C('garant-sklad.txt');
+//$test->parseGarant();
